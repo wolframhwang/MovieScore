d99fea7a100bb36a6527476f0ce873a1ddd59e93
wolframhwang <hzw94@naver.com>
2022-04-13 01:02:13 +0900

    [fix] Manager: request headers 오타 정정

--- MovieScore/Manager/MovieSearchManager.swift
+++ MovieScore/Manager/MovieSearchManager.swift
@@ -8,13 +8,17 @@
 import Foundation
 import Alamofire
 
+protocol MovieSearchManagerProtocol {
+    func request(from keyword: String, completionHandler: @escaping (([Movie]) -> Void))
+}
+
 struct MovieSearchManager {
     func request(from keyword: String, completionHandler: @escaping (([Movie]) -> Void)) {
         guard let url = URL(string: "https://openapi.naver.com/v1/search/movie.json")
         else { return }
         
         let parameters = MovieSearchRequestModel(query: keyword)
-        let headers: HTTPHeaders = ["X-Naver-Clienr-Id": "oh0vdfPIvFpC9Wm1BMA5", "X-Naver-Client-Secret": "sf16eYZg_j"]
+        let headers: HTTPHeaders = ["X-Naver-Client-Id": "oh0vdfPIvFpC9Wm1BMA5", "X-Naver-Client-Secret": "sf16eYZg_j"]
         
         AF.request(url, method: .get, parameters: parameters, headers: headers).responseDecodable(of: MovieSearchResponseModel.self) { response in
             switch response.result {
