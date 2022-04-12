cef190a450fc228ddf5d01f11d5602cfbfd3f3ae
wolframhwang <hzw94@naver.com>
2022-04-13 00:04:48 +0900

    [chore] SwiftLint 체험하기 위한 수정사항 반영

--- MovieScore/AppDelegate.swift
+++ MovieScore/AppDelegate.swift
@@ -24,5 +24,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
+    
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        
+    }
 }
 
