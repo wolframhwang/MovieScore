a4cf53bbf0a0d27204c3171a5d1f3887dd6a4f7b
wolframhwang <hzw94@naver.com>
2022-04-13 00:53:02 +0900

    [feat] Manager: MovieSearchManager에 사용할 request 작성 (completionHandler 적용 전)

--- MovieScore/Manager/MovieSearchManager.swift
+++ MovieScore/Manager/MovieSearchManager.swift
@@ -8,4 +8,21 @@
 import Foundation
 import Alamofire
 
-
+struct MovieSearchManager {
+    func request(from keyword: String) {
+        guard let url = URL(string: "https://openapi.naver.com/v1/search/movie.json")
+        else { return }
+        
+        let parameters = MovieSearchRequestModel(query: keyword)
+        let headers: HTTPHeaders = ["X-Naver-Clienr-Id": "oh0vdfPIvFpC9Wm1BMA5", "X-Naver-Client-Secret": "sf16eYZg_j"]
+        
+        AF.request(url, method: .get, parameters: parameters, headers: headers).responseDecodable(of: MovieSearchResponseModel.self) { response in
+            switch response.result {
+            case .success(let result):
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
