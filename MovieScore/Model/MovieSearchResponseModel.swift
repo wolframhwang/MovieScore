04938edba791b847b9695c350d493126e43275f9
wolframhwang <hzw94@naver.com>
2022-04-13 00:44:55 +0900

    [feat] model: Movie 추가

--- MovieScore/Model/MovieSearchResponseModel.swift
+++ MovieScore/Model/MovieSearchResponseModel.swift
@@ -8,3 +8,15 @@
 import Foundation
 
 //MovieSearchResponseModel
+struct MovieSearchResponseModel: Decodable {
+    var items: [Movie] = []
+}
+
+struct Movie: Decodable {
+    let title: String
+    let image: String
+    let userRating: String
+    let actor: String
+    let director: String
+    let pubDate: String
+}
