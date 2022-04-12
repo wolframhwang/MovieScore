0b4f0e3eef7fe30bc4b9f7848dafc2fa7a09eaaf
wolframhwang <hzw94@naver.com>
2022-04-13 01:02:34 +0900

    [test] Search Manager 정상 작동 확인

--- MovieScore/ViewController.swift
+++ MovieScore/ViewController.swift
@@ -11,7 +11,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        MovieSearchManager().request(from: "Starwars") { movies in
+            print(movies)
+        }
     }
 
 
