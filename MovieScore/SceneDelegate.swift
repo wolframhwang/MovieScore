83fcaf2c89d304129c465a63de82ed69f45387e5
wolframhwang <hzw94@naver.com>
2022-04-13 22:24:10 +0900

    [feat] NavigationViewController 로 MovieScene 임베드

--- MovieScore/SceneDelegate.swift
+++ MovieScore/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: windowScene)
         window?.backgroundColor = .systemBackground
-        window?.rootViewController = MovieListViewController()
+        window?.rootViewController = UINavigationController(rootViewController: MovieListViewController())
         
         window?.makeKeyAndVisible()
     }
